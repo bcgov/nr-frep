cc04681a64a8474b57b35a0de12142f1d9f01c7d
Avisha Sodhi <38086281+SodhiA1@users.noreply.github.com>
2026-06-01 10:56:11 -0700

    feat: Test deployment

--- backend/src/main/java/ca/bc/gov/nrs/frep/service/AcceptedSiteService.java
+++ backend/src/main/java/ca/bc/gov/nrs/frep/service/AcceptedSiteService.java
@@ -3,6 +3,7 @@ package ca.bc.gov.nrs.frep.service;
 import ca.bc.gov.nrs.frep.dto.AcceptedSiteResponse;
 import java.util.List;
 
+//Test deploy
 public interface AcceptedSiteService {
 
   List<AcceptedSiteResponse> findAcceptedSites(

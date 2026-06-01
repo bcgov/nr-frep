96d245dc597916b4a9a8f36bc420091d6c454784
Avisha Sodhi <38086281+SodhiA1@users.noreply.github.com>
2026-06-01 10:57:17 -0700

    Revert "feat: Test deployment"
    This reverts commit cc04681a64a8474b57b35a0de12142f1d9f01c7d.

--- backend/src/main/java/ca/bc/gov/nrs/frep/service/AcceptedSiteService.java
+++ backend/src/main/java/ca/bc/gov/nrs/frep/service/AcceptedSiteService.java
@@ -3,7 +3,6 @@ package ca.bc.gov.nrs.frep.service;
 import ca.bc.gov.nrs.frep.dto.AcceptedSiteResponse;
 import java.util.List;
 
-//Test deploy
 public interface AcceptedSiteService {
 
   List<AcceptedSiteResponse> findAcceptedSites(

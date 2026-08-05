629096eb5592fbc104d1aa7450c5322e6236bb27
Avisha Sodhi <38086281+SodhiA1@users.noreply.github.com>
2026-08-05 15:15:44 -0700

    Update ProtocolChecklistServiceTest.java

--- backend/src/test/java/ca/bc/gov/nrs/frep/service/v1/frep/ProtocolChecklistServiceTest.java
+++ backend/src/test/java/ca/bc/gov/nrs/frep/service/v1/frep/ProtocolChecklistServiceTest.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
